87ec8ac54de8147413ea487691b8da5954e3fb52
Jorge Vargas <jorgevargaslarrota@hotmail.com>
2014-03-04 15:48:20 -0500

    Update richsnippets.php
    Standards update delete white spaces

--- richsnippets/richsnippets.php
+++ richsnippets/richsnippets.php
@@ -23,7 +23,7 @@
 *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
-   
+
 if (!defined('_PS_VERSION_'))
     exit;
 
@@ -37,28 +37,28 @@ class RichSnippets extends PaymentModule
         $this->version = '0.1';
         $this->need_instance = 1;
         $this->module_key = '';
-            
+
         parent::__construct();
-            
+
         $this->displayName = 'Rich Snippets';
         $this->description = 'Add meta data for rich snippets';
-            
+
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
-            
+
         $this->ps_versions_compliancy = array('min' => '1.5.0.1', 'max' => '1.6.0.0');
     }
-        
+
     /* Install */
     public function install()
     {
         if (Shop::isFeatureActive())
             Shop::setContext(Shop::CONTEXT_ALL);
-        
+
         return (parent::install() &&
         $this->registerHook('header') &&
         $this->registerHook('productfooter'));
     }
-        
+
     /* Uninstall */
     public function uninstall()
     {
@@ -66,18 +66,18 @@ class RichSnippets extends PaymentModule
             return false;
         return true;
     }
-    
+
     public function displayBlock($display = 'header')
     {
         $id_lang = $this->context->language->id;
         $id_shop = $this->context->shop->id;
-        
+
         $product = new Product((int)Tools::getValue('id_product'), false, $id_lang, $id_shop);
-        
+
         if (Validate::isLoadedObject($product))
         {
             $manufacturer = new Manufacturer($product->id_manufacturer, $id_lang);
-            
+
             //image of the product
             $id_image = Product::getCover((int)Tools::getValue('id_product'));
             if (count($id_image) > 0)
@@ -88,7 +88,7 @@ class RichSnippets extends PaymentModule
                 'image' => $image,
                 'manufacturer' => $manufacturer,
                 ));
-                
+
             // Shows tpl file
             if ($display == 'header')
                 return $this->display(__FILE__, 'header.tpl');
@@ -99,15 +99,14 @@ class RichSnippets extends PaymentModule
             return $this->display(__FILE__, 'header.tpl');
 
     }
-    
+
     public function hookDisplayHeader()
     {
         return $this->displayBlock('header');
     }
-    
+
     public function hookDisplayFooterProduct()
     {
         return $this->displayBlock('footerproduct');
     }
-        
 }
